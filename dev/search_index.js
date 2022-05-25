aa897054657377ba833af9b55c18f0bfcbbdd6ef
Documenter.jl <documenter@juliadocs.github.io>
2022-05-25 20:37:24 +0000

    build based on 354b0d4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DEIM","category":"page"},{"location":"#DEIM","page":"Home","title":"DEIM","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DEIM.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DEIM]","category":"page"}]
+}
