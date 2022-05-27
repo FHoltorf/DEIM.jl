f08d8bca8bf485bac43558805425ecc9238ce3e4
Flemming Holtorf <holtorf@mit.edu>
2022-05-27 09:30:26 -0400

    rm plotting

--- test/arrhenius_term.jl
+++ test/arrhenius_term.jl
@@ -1,11 +1,12 @@
-using ModelingToolkit, LinearAlgebra, GLMakie
+using ModelingToolkit, LinearAlgebra
 
 N = 1000
 # scaled temperature profile: ν = T/T_ref with T_ref = (Ea/R)
 ν(i,N,t) = (0.5 + 0.5*sin(2π*t)^2)*(3/2-exp(-i/N)) 
 # arrhenius term evaluated for non-dimensionalized temperature
-r(ν) = exp.(-ν.^2) +  exp()
+r(ν) = exp.(-ν.^2) 
 
+# collect data
 t_range = 0:0.01:1
 R = zeros(N, length(t_range))
 for (k,t) in enumerate(t_range)
@@ -18,6 +19,7 @@ m = findfirst(x -> x <= 1e-8, S)
 @variables temp[1:N]
 temp = collect(temp)
 
+# compute DEIM interpolation
 Πinv, red_r, red_vars =  DEIM_interpolation(U[:, 1:m], r, temp)
 red_var_indices = [arguments(var.val)[end] for var in red_vars]
 
@@ -29,9 +31,3 @@ for (k,t) in enumerate(t_range)
 end
 
 @test norm(R - R_red) <= 1e-8
-
-vis_comparison = Figure()
-ax = Axis3(vis_comparison[1,1])
-surface!(ax, 1:N, t_range, R, color = fill("#6d7d8a80", 1,1), transparency = true)
-di, dt = 10, 1
-wireframe!(ax, 1:di:N, t_range[1:dt:end], R_red[1:di:end, 1:dt:end], color = :blue, overdraw = true, tansparency = true)
